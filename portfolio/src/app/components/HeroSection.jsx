8cdd6b3a51a1c9da45dbf1a91de2ba6f1adf5915
Cody Taylor Jenkins <jenkinscodytaylor@gmail.com>
2023-12-14 16:54:17 -0500

    first commit

---
+++ portfolio/src/app/components/HeroSection.jsx
@@ -0,0 +1,57 @@
+"use client";
+import React from 'react'
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+import Link from "next/link";
+
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">Hi, I'm
+                <br></br>
+                <TypeAnimation
+                    sequence={[
+                        'Cody Taylor Jenkins',
+                        1000,
+                        'Web Developer',
+                        1000,
+                        'Frontend Developer',
+                        1000,
+                        'UI/UX Designer',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    repeat={Infinity}
+                    />               
+                </h1>
+                    <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>Welcome to my page where I'll be sucking dick and cock all summer long.
+                    </p>
+                    <div>
+                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-black'>Hire Me</button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
+                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+                        </button>
+                    </div>
+    
+            </div>
+            <div className='col-span-5'>
+                <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+                    <Image
+                        src="/images/profile.png"
+                        alt="hero_image"
+                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        width={300}
+                        height={300}
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+};
+
+export default HeroSection
