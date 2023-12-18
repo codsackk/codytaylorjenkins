e065f39d918327760ef0c1f411ee0ccfc69cc468
Cody Taylor Jenkins <jenkinscodytaylor@gmail.com>
2023-12-18 09:31:30 -0500

    first commit

--- portfolio/src/app/components/Navbar.jsx
+++ portfolio/src/app/components/Navbar.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React, {useState} from 'react';
 import NavLink from "./NavLink";
-import { Bars3Icon, XmarkIcon} from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
@@ -28,7 +28,7 @@ const Navbar = () => {
             <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
                 <Link 
                     href={"/"} 
-                        className="text-5xl text-white font-semibold"
+                        className="text-2xl md:text-5xl text-white font-semibold"
                         >
                     LOGO
                 </Link>
@@ -42,20 +42,18 @@ const Navbar = () => {
                         <button
                         onClick={() => setNavbarOpen(false)}
                         className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
-                            <XmarkIcon className="h-5 w-5" />
+                            <XMarkIcon className="h-5 w-5" />
                         </button>
                     )}
                 </div>
-                <div className="menu hidden block md:w-auto" id="navabar">
+                <div className="menu hidden md:block md:w-auto" id="navbar">
                     <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                         {
                             navLinks.map((link, index) => (
                                 <li key={index}>
-                                    <NavLink href={link.path}> title={link.title}</NavLink>
+                                    <NavLink href={link.path} title={link.title}/>
                                 </li>
-                            )
-                            )
-                        }
+                            ))}
                     </ul>
                 </div>
             </div>
